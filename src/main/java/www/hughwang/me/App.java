b700e1efe9792783a4ad9018ac8f74f0db6ceb11
wanghuan <hunter.v.wang@gmail.com>
2017-07-25 14:59:45 +0800

    sd

---
+++ src/main/java/www/hughwang/me/App.java
@@ -0,0 +1,13 @@
+package www.hughwang.me;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
